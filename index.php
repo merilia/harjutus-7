1f4919aa7d010a0b6f5769cb360ca20ac2364e85
Merili <meriliasu@gmail.com>
2014-09-23 17:04:05 +0300

    Add student exercise
    Add student exercise

--- index.php
+++ index.php
@@ -102,6 +102,62 @@
 	?> 
 	-->
 	<h2>For loop</h2>
+	<?php
+      for ($count = 1; $count <= 20; $count += 1) {
+        if ($count % 2 == 0) {
+            echo "Number $count on paarisarv.";
+            echo "<br>";
+          } 
+          elseif ($count % 2 == 1) {
+            echo "Number $count on paaritu arv.";
+            echo "<br>";
+          } 
+          $count % 2;
+        }
+       $animals = array("elephant", "dog", "cat", "fox");
+
+       foreach ($animals as $animal) {
+        	echo $animal;
+    }
+	?> 
+	<h2>Foreach loop</h2>
+  	<?php
+  		$students = array("Marko", "Fränk", "Merili", "Merili", "Triinu", "Veronika", "Silja", "Kristi", "Markus", "Cärolin", "Heiko", "Ingrid", "Syret", "Mihkel", "Eve", "Anneli", "Ants", "Risto", "Marge", "Aile");
+  		$nr = 1;
+  		foreach ($students as $student) {
+        	echo "$nr.  Õpilase nimi on: $student ";
+        	$nr++;
+    	}
+    	 $companies = array("Razer", "Roccat", "Asus", "Beats", "Apple");
+	      echo "<ul>";
+	      foreach ($companies as $company) {
+		  echo "<li>".$company."</li>";
+      }
+      	  echo "</ul>";
+    	$subjects = array("Serveripoolsete veebirakenduste programmeerimine", "Veebikujundus", "Sisuhaldussüsteemide kasutamine", "3D alused", "Veebiarendus");
+              $number = 1;
+              echo "<tr>";
+              foreach ($subjects as $subject) { 
+              echo "<td>".$number.". ".$subject."</td><br>";
+              $number += 1;
+      }
+     	  echo "</tr>";
+  	?>
+  	<br>
+    <?php
+       $contact = array(
+            "first_name" => "Merili", 
+            "last_name" => "Asu", 
+            "e-mail" => "merili.asu@khk.ee",
+            "age" => 21
+    );
+         	echo "<table class='contact-table'><tbody>";
+              foreach ($contact as $attribute => $value) {
+              $attribute_formatted = ucfirst(str_replace("_", " ", $attribute));
+          	echo "<tr><td class='first-column'>".$attribute_formatted."</td> "."<td>".$value."</td></tr>"; 
+      };
+        	echo "</tbody></table>";
+  	?>
 </body>
 </html>
 		
